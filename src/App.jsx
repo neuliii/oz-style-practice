0c6920c1a0582326220d07c75425dcb0ab362272
neuliii <phn01113@gmail.com>
2025-02-20 16:43:12 +0900

    💄 SCSS style을 styled-components 로 변경 적용

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,25 @@ import "./App.scss";
 import { contents } from "./assets/data/data";
 import Content from "./components/Content";
 import Header from "./components/Header";
+import styled from "styled-components"
+import { flexMixin } from "./styled/styled";
+
+
+
+const SectionStyled = styled.section`
+  ${flexMixin({justify: "center", align: "center", wrap: 'wrap', gap: '20px'})}
+  padding: 20px 40px;
+`;
 
 function App() {
   return (
     <main>
       <Header />
-      <section>
+      <SectionStyled>
         {contents.map((el) => (
           <Content key={el.id} content={el} />
         ))}
-      </section>
+      </SectionStyled>
     </main>
   );
 }
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,10 +1,39 @@
+import styled from "styled-components";
+import { flexMixin, fontMixin, gray, tagColor } from "../styled/styled";
+
+
+const ContentStyled = styled.div`
+  ${flexMixin({direction: 'column', align: "center", gap: '5px'})}
+  img {
+    width: 300px;
+    border-radius: 10px;
+    margin-bottom: 3px;
+    min-height: 225px;
+  }
+  span {
+    ${fontMixin({size: '12px'})}
+    color: ${tagColor};
+    border: 1px solid ${tagColor};
+    padding: 4px 5px;
+    border-radius: 3px;
+  }
+  div {
+    ${fontMixin({size: '18px', weight: '600'})}
+  }
+  p {
+
+    ${fontMixin({size: '12px'})}
+    color: ${gray};
+  }
+`;
+
 export default function Content({ content }) {
   return (
-    <div className="content-container">
+    <ContentStyled>
       <img src={content.img} alt={content.title} />
       <span>모집중</span>
       <div>{content.title}</div>
       <p>{content.subtitle}</p>
-    </div>
+    </ContentStyled>
   );
 }
